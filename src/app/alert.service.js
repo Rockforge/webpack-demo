d372628bc7a776ab15352cd17c8d47a37d133b6b
Christian Jurilla <cjurilla@optimy.com>
2021-09-17 22:26:13 +0800

    Finished import and export of js files

--- src/app/alert.service.js
+++ src/app/alert.service.js
@@ -1,10 +1,12 @@
 import { inputsAreValid } from "./utils/inputs-are-valid";
+
 export class AlertService {
   constructor() {
     this.errorBox = document.getElementById("error");
   }
 
   handleAdditionError(inputs, numbers) {
+
     const fullMessage = inputs.reduce((message, str, index) => {
       if (inputsAreValid(numbers[index])) {
         return message + "";
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import { run } from "./app/app";
-import "./main.scss";
-import { AlertService } from "./app/alert.service";
-import { ComponentService } from "./app/component.service";
+import { AlertService } from "./app/alert.service.js";
+import { ComponentService } from "./app/component.service.js";
+
 const alertService = new AlertService();
 const componentService = new ComponentService();
+
 run(alertService, componentService);
-console.log("J");
